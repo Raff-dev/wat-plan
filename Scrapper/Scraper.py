e2ac890b8fac9da5d9cd1ad58b56c953e41465a0
Raff <56380303+Raff-dev@users.noreply.github.com>
2020-04-01 16:14:02 +0200

    scraper optimization

--- Scrapper/Scraper.py
+++ Scrapper/Scraper.py
@@ -1,24 +1,29 @@
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import NoSuchElementException
-import re
-import time
-import sys
-
-from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from threading import Thread
 from datetime import date, timedelta, datetime
 import pandas as pd
 import numpy as np
+import re
+import time
+import sys
 from creds import username, password
 
 class Scraper():
     def __init__(self):
         options = Options()
-        options.add_argument("--headless")
+        # options.add_argument("--headless")
         self.bot = webdriver.Chrome(ChromeDriverManager().install(),options=options)
         self.in_groups=False
         self.scrape_ready=False
+        self.test = False
+
+    def __repr__(self):
+        return self.bot
 
     # method -> 'class name' 'id' 'xpath' 'link text' 'name' 'css selector'
     def find(self, method, element: str):   
@@ -70,13 +75,16 @@ class Scraper():
         self.in_groups = True
 
     def scrape_semester(self,group_name):
-        prev = datetime.now()
+        scrape_start = datetime.now()
         assert self.in_groups, 'Groups page must be opened in order to scrape'
         group_link = self.find('link text', group_name)
         if group_link:
             group_link.click()
             self.scrape_ready = True
+        page_loaded = datetime.now()
+        print(f'Loading Page took {(page_loaded-scrape_start).seconds} seconds')
 
+        soup = BeautifulSoup(self.bot.page_source, features='lxml')
         roman_notation={
             'I':1,'II':2,'III':3,'IV':4,'V':5,'VI':6,'VII':7,'VIII':8,'IX':9,'X':10,'XI':11,'XII':12
         }
@@ -84,43 +92,46 @@ class Scraper():
         day,month = day[0].get_attribute('innerText').split('\n')
         month = roman_notation[month]
         day = date(date.today().year,month,int(day))
-
         data={}
-        for plan_day in self.scrape_day():
+
+        for plan_day in self.scrape_day(soup):
             data[str(day)] = plan_day
             day+=timedelta(days=1)
-        print(f'Scraping {group_name} took {(datetime.now()-prev).seconds} seconds')
+
+        print(f'Scraping {group_name} took {(datetime.now()-page_loaded).seconds} seconds')
+        print(f'Total time: {(datetime.now()-scrape_start).seconds} seconds')
         return data
 
-    def scrape_day(self):
+    def scrape_day(self,soup):
         """this is a generator function yelding one day of whole plan"""
-        assert self.scrape_ready, 'Plan of a group bust be opened in order to scrape'
+        assert self.scrape_ready, 'Plan of a group must be opened in order to scrape'
 
         # finding all the block cells in a plan & rearanging them for easier manipulation
-        cells = self.find('class name', 'tdFormList1DSheTeaGrpHTM3')
         days,blocks_per_day =7,7
-        cells = np.array_split(cells,days*blocks_per_day)
+        cells = soup.find_all('td',class_='tdFormList1DSheTeaGrpHTM3')
+        cells = pd.Series(cells)
+        cells = np.array_split(cells.values,days*blocks_per_day)
         df = pd.DataFrame(cells).T
-         
         day_data = {}
         for week in df.values:
             for block, i in zip(week,range(len(week))):
-                # i%7 and i!=0 means all 7 blocks of a day
-                # have been gathered and are ready to yield
-                if i % 7 == 0 and i:
-                    yield day_data
-                    day_data.clear()
-
-                data = block.get_attribute('innerText').split('\n')
-                block_data = {}
-                if len(data) >= 5:
-                    block_data['info'] = block.get_attribute('title')
+                if block==None or len(block.text) == 1:
+                    day_data[ i%7 +1 ] = None
+                else:
+                    block_data = {}
+                    data = block.find_all('nobr')
+                    block_data['title'] = block['title']
+                    block_data['teacher'] = data[1].text
+                    block_data['index'] = data[2].text.replace('[','').replace(']','')
+                    data = str(data[0]).replace('<br/>','|')
+                    data = BeautifulSoup(data,features="lxml").nobr.text.split('|')
                     block_data['subject'] = data[0]
-                    block_data['class_type'] = data[1]
-                    block_data['teacher'] = data[-2]
-                    block_data['index'] = data[-1]
-                    block_data['place'] = data[2:-2]
-                day_data[i % 7 +1] = block_data if len(block_data) else None
+                    block_data['class_type'] = data[1].replace('(','').replace(')','')
+                    block_data['place'] = data[2:]
+                    day_data[ i%7 +1 ] = block_data
+
+                if i and (i+1) % 7 == 0 :
+                    yield day_data.copy()
 
     def get_current_url(self):
         return self.bot.current_url
--- Scrapper/Scraper_Controller.py
+++ Scrapper/Scraper_Controller.py
@@ -4,5 +4,10 @@ scraper = Scraper.Scraper()
 scraper.login()
 scraper.get_to_groups('letni')
 group_names = scraper.get_group_names()
-scraper.find('link text', next(group_names)).click()
-scraper.scrape()
+data = {}
+for group in group_names:
+    data[group] = scraper.scrape_semester(group)
+
+def get(group,month,day):
+    for k,v in data[group][f'2020-{month}-{day}'].items():
+        print(k,v)
