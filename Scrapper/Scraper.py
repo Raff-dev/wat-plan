4f611353faeebfb836f1b928b1a9da986eedc6b4
Raff <56380303+Raff-dev@users.noreply.github.com>
2020-04-02 00:25:44 +0200

    multiprocessed scraping

--- Scrapper/Scraper.py
+++ Scrapper/Scraper.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from threading import Thread
+from multiprocessing import Process
 from datetime import date, timedelta, datetime
 import pandas as pd
 import numpy as np
@@ -14,16 +15,46 @@ import sys
 from creds import username, password
 
 class Scraper():
-    def __init__(self):
-        options = Options()
-        # options.add_argument("--headless")
-        self.bot = webdriver.Chrome(ChromeDriverManager().install(),options=options)
-        self.in_groups=False
-        self.scrape_ready=False
-        self.test = False
 
-    def __repr__(self):
-        return self.bot
+    def __init__(self,headless=True,bot_init=False,semester=None, index = None):
+        self.options = Options()
+        self.options.add_argument("--headless") if headless else None
+        self.bot = self.get_bot() if bot_init == True else None
+        self.semester = semester
+        self.index = index
+
+        self.groups = None
+        self.url = {'letni':None,'zimowy':None}
+        self.data = {}
+
+        self.pool_size = None
+        self.queue=None
+
+        self.current_group = None
+        self.groups_scraped = 0
+        self.timer = get_timer()
+
+    def get_bot(self):
+        return webdriver.Chrome(ChromeDriverManager().install(),options=self.options)
+
+    def set_up(self,semester = None, url = None, groups = None, pool_size=None,):
+        self.semester = semester if semester else self.semester
+        self.url[self.semester] = url if url else self.url[self.semester]
+        self.groups = groups if groups else self.groups
+        self.pool_size = pool_size if pool_size else self.pool_size
+        assert self.url[self.semester] or url, 'Scraper must have an url'
+
+    def run(self):
+        self.bot = self.get_bot() if not self.bot else None
+        self.get(self.url[self.semester])
+        if not self.groups:
+            self.get_groups()
+        if self.index and self.pool_size:
+            self.groups = np.array_split(self.groups,self.pool_size)[self.index]
+        for group in self.groups:
+            self.scrape_semester(group)
+        self.time_finish = datetime.now()-self.time_born
+        print(f'Scraper-{self.index} finished within {self.time_finish}')
 
     # method -> 'class name' 'id' 'xpath' 'link text' 'name' 'css selector'
     def find(self, method, element: str):   
@@ -35,8 +66,14 @@ class Scraper():
                 send_msg += 1
                 if not send_msg % 20:
                     print('Trying to find the element')
-                    assert send_msg < 100, 'Could not find element: ' + element
-                time.sleep(0.1)
+                    if send_msg > 10:
+                        print('Could not find element: ' + element)
+                        print('Refreshing page')
+                        self.get(self.curl)
+                    if send_msg > 30:
+                        print('Could not find element: ' + element)
+                        return None
+                time.sleep(0.5)
         return el if len(el) > 1 else el[0]
 
     def login(self):
@@ -49,15 +86,9 @@ class Scraper():
 
         self.find('css selector', 'input.inputLogL').click()
 
-    def get_group_names(self):
-        group_names = []
-        aMenus = self.find('class name', "aMenu")
-        for aMenu in aMenus:
-            group_names.append(aMenu.get_attribute('innerText'))
-        return group_names
-
     def get_to_groups(self, semester):
         self.in_groups=False
+        self.semester = semester
         menus = self.find('class name', 'ThemeIEMainFolderText')
         schedules = self.find('class name', 'ThemeIEMenuFolderText')
         semesters = self.find('class name', 'ThemeIEMenuItemText')
@@ -73,16 +104,27 @@ class Scraper():
         for button in [menus[1], schedules[2], sems[2]]:
             button.click()
         self.in_groups = True
+        self.url[self.semester] = self.curl()
+
+    def get_groups(self):
+        groups = []
+        aMenus = self.find('class name', "aMenu")
+        for aMenu in aMenus:
+            groups.append(aMenu.get_attribute('innerText'))
+
+        self.groups = groups
+        return groups
 
     def scrape_semester(self,group_name):
+        self.current_group = group_name
         scrape_start = datetime.now()
-        assert self.in_groups, 'Groups page must be opened in order to scrape'
         group_link = self.find('link text', group_name)
-        if group_link:
-            group_link.click()
-            self.scrape_ready = True
+        if group_link==None:
+            self.data[group_name] = None
+            return
+        group_link.click()
         page_loaded = datetime.now()
-        print(f'Loading Page took {(page_loaded-scrape_start).seconds} seconds')
+        delta_loading = (page_loaded-scrape_start).microseconds
 
         soup = BeautifulSoup(self.bot.page_source, features='lxml')
         roman_notation={
@@ -98,9 +140,10 @@ class Scraper():
             data[str(day)] = plan_day
             day+=timedelta(days=1)
 
-        print(f'Scraping {group_name} took {(datetime.now()-page_loaded).seconds} seconds')
-        print(f'Total time: {(datetime.now()-scrape_start).seconds} seconds')
-        return data
+        delta_scraping = (datetime.now()-page_loaded).microseconds
+        delta_total = delta_loading + delta_scraping
+        self.notify(delta_loading,delta_scraping,delta_total)
+        self.data[group_name] = data.copy()
 
     def scrape_day(self,soup):
         """this is a generator function yelding one day of whole plan"""
@@ -132,9 +175,53 @@ class Scraper():
 
                 if i and (i+1) % 7 == 0 :
                     yield day_data.copy()
+    
 
-    def get_current_url(self):
+    def curl(self):
         return self.bot.current_url
+
+    def notify(self,loading,scraping,total):
+        self.timer['loading'] += loading
+        self.timer['scraping'] += scraping
+        self.timer['total'] += total
+        self.groups_scraped += 1
+
+        if self.queue:
+            self.queue.put({
+                'loading' : loading,
+                'scraping' : scraping,
+                'total' : total,
+                'index' : self.index,
+                'semester' : self.semester,
+                'groups_scraped' : self.groups_scraped,
+                'group' : self.current_group
+                })
+        else:
+            loading = self.timer['loading']
+            scraping = self.timer['scraping']
+            total = self.timer['total']
+            born = self.timer['born']
+            print(f'Average time for {self.semester}-{self.index}')
+            print(f'Loading: {round(loading/self.groups_scraped/100000,2)} seconds')
+            print(f'Scraping: {round(scraping/self.groups_scraped/100000,2)} seconds')
+            print(f'Total: {round(total/self.groups_scraped/100000,2)} seconds')
+            print(f'Time alive: {datetime.now()-born}')
+            print('')
+
+    
+    def get(self,url):
+        self.in_groups=True
+        self.bot.get(url)
 
     def close(self):
         self.bot.close()
+
+def get_timer():
+        timer ={}
+        for name in ['born','alive','loading','scraping','total','finish']:
+            if name =='born': timer[name] = datetime.now() 
+            else: timer[name] = 0
+        return timer.copy()
+
+def post_result(data):
+        print('posting')
