1a40351b2e96ade8aad8f5fbdad833ae4fc52925
Raff <56380303+Raff-dev@users.noreply.github.com>
2020-04-04 01:22:20 +0200

    API is working

--- API/Plan/models.py
+++ API/Plan/models.py
@@ -1,22 +1,55 @@
 from django.db import models
 
+
 class Group(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(unique=True, max_length=30)
+
+    def __str__(self):
+        return self.name
+
 
 class Semester(models.Model):
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
-    name = models.PositiveSmallIntegerField(choices=[(1,'Zimowy'),(2,'Letni')])
+    group = models.ForeignKey(
+        Group, on_delete=models.CASCADE, related_name='semesters')
+    name = models.CharField(max_length=30)
+    empty = models.BooleanField(default=False)
+    version = models.IntegerField(default=0)
 
-class Day(models.Model):
-    semester = models.ForeignKey(Semester,on_delete=models.CASCADE)
-    date = models.DateField()
 
 class Block(models.Model):
-    day = models.ForeignKey(Day, on_delete=models.CASCADE)
-    subject = models.CharField(max_length=30)
-    teacher = models.CharField(max_length=30)
-    place = models.CharField(max_length=30)
-    class_type = models.CharField(max_length=30)
-    number = models.IntegerField()
+    semester = models.ForeignKey(
+        Semester, on_delete=models.CASCADE, related_name='blocks')
+    date = models.DateField(max_length=10)
+    index = models.IntegerField()
+    empty = models.BooleanField(default=False)
 
+    title = models.CharField(
+        default=None, max_length=100, null=True, blank=True)
+    subject = models.CharField(
+        default=None, max_length=30, null=True, blank=True)
+    teacher = models.CharField(
+        default=None, max_length=30, null=True, blank=True)
+    place = models.CharField(
+        default=None, max_length=30, null=True, blank=True)
+    class_type = models.CharField(
+        default=None, max_length=30, null=True, blank=True)
+    class_index = models.CharField(
+        max_length=1, default=None, null=True, blank=True)
 
+    class Meta():
+        unique_together = ['semester', 'date', 'index']
+        ordering = ['semester__group__name', 'semester__name', 'date', 'index']
+
+    # pervious_values = None
+
+    # def __init__(self, *args, **kwargs):
+    #     super(Block, self).__init__(*args, **kwargs)
+    #     self.pervious_values = kwargs
+
+    # def save(self, force_insert=False, force_update=False, *args, **kwargs):
+    #     super(Block, self).save(force_insert, force_update, *args, **kwargs)
+    #     if self.pervious_values != kwargs:
+    #         group = Group.objects.get(name=self.group)
+    #         group.version += 1
+    #         group.save()
+    #     self.pervious_values = kwargs
