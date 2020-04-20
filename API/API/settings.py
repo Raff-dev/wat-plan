9788d1412178a6a730a0dcb222a635242d8e9552
Raff <56380303+Raff-dev@users.noreply.github.com>
2020-04-20 13:50:56 +0200

    config fix

--- API/API/settings.py
+++ API/API/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['10.0.2.2', '127.0.0.1', 'watplan.eu.pythonanywhere.com']
 
