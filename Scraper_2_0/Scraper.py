6569de9423bc48b33a9cfd3f58b459c2623ff4f7
Raff <56380303+Raff-dev@users.noreply.github.com>
2020-10-06 18:40:45 +0200

    Reduced Scraper class responsibility to obtaining data only

--- Scraper_2_0/Scraper.py
+++ Scraper_2_0/Scraper.py
@@ -1,9 +1,14 @@
 import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
 from typing import List, Dict
+from bs4 import BeautifulSoup
+import time
 
 from form_data import FORM_DATA
-from Decorators import HasRequiredAttribute, TimeMeasure
+from Decorators import timer
 from SoupParser import SoupParser
+from Setting import Setting
 
 BASE_URL = 'https://s1.wcy.wat.edu.pl/ed1/'
 
@@ -12,11 +17,10 @@ GROUPS_URL_ADDON = '&mid=328'
 GROUP_URL_ADDON = '&exv='
 SEMESTER_URL_ADDON = '&iid='
 
-WINTER, SUMMER, RETAKE = '1', '2', '3'
-SID, SEMESTER, YEAR, GROUP = 'SID', 'SEMESTER', 'YEAR', 'GROUP'
 
+class Scraper():
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-class Scraper(HasRequiredAttribute):
     """
     Contains all the needed functionalities to
     obtain access to the schedule website,
@@ -24,38 +28,42 @@ class Scraper(HasRequiredAttribute):
     to the WAT Plan API
     """
 
-    def __init__(self, **kwargs):
-        self.SID = None
-        self.YEAR = None
-        self.SEMESTER = None
-        self.GROUP = None
-        self.set_attributes(**kwargs)
+    @staticmethod
+    def get_soup(url: str) -> BeautifulSoup:
+        res = requests.get(url, verify=False)
+        assert res.status_code == requests.codes.ok, (
+            f'Failed to obtain soup, request status: {res.status_code}')
 
-    @property
-    @HasRequiredAttribute.requires_attribute(SID, SEMESTER, YEAR)
-    def groups_url(self) -> str:
+        # --- TEST PRINT ---
+        print(res)
+        soup = BeautifulSoup(res.text, features="lxml")
+        return soup
+
+    @staticmethod
+    @Setting.requires_setting(Setting.SID, Setting.SEMESTER, Setting.YEAR)
+    def get_groups_url(setting: Setting) -> str:
         """
         Returns an url path of a site containing all groups
         of current year and semester.
         """
 
-        return BASE_URL + LOGGED_URL_ADDON + self.SID + GROUPS_URL_ADDON +\
-            SEMESTER_URL_ADDON + self.YEAR + self.SEMESTER
+        return BASE_URL + LOGGED_URL_ADDON + setting.sid + GROUPS_URL_ADDON +\
+            SEMESTER_URL_ADDON + setting.year + setting.semester
 
-    @property
-    @HasRequiredAttribute.requires_attribute(SID, SEMESTER, YEAR, GROUP)
-    def group_url(self) -> str:
+    @staticmethod
+    @Setting.requires_setting(Setting.SID, Setting.SEMESTER, Setting.YEAR, Setting.GROUP)
+    def get_group_url(setting: Setting) -> str:
         """
         Returns an url path of a site containing current group's plan.
         """
-        return self.groups_url + GROUP_URL_ADDON + self.GROUP
+        return Scraper.get_groups_url(setting) + GROUP_URL_ADDON + setting.group
 
     @staticmethod
     def authenticate() -> str:
         """ Logs into website and obtains sid """
 
         # make sure the website isnt offline
-        form = SoupParser.get_soup(BASE_URL).form
+        form = Scraper.get_soup(BASE_URL).form
         login_url = BASE_URL + form['action']
         res = requests.post(login_url, data=FORM_DATA, verify=False)
 
@@ -64,11 +72,13 @@ class Scraper(HasRequiredAttribute):
         # make sure it has logged in succesfully
         # check the title
 
-    @HasRequiredAttribute.requires_attribute(SID, SEMESTER, YEAR)
-    def get_groups(self) -> List[str]:
+    @staticmethod
+    @Setting.requires_setting(Setting.SID, Setting.SEMESTER, Setting.YEAR)
+    def get_group_names(setting: Setting) -> List[str]:
         """gets all available groups of a given semester"""
 
-        soup = SoupParser.get_soup(self.groups_url)
+        url = Scraper.get_groups_url(setting)
+        soup = Scraper.get_soup(url)
         aMenus = soup.find_all("a", class_='aMenu')
 
         groups = []
@@ -77,30 +87,17 @@ class Scraper(HasRequiredAttribute):
 
         return groups
 
-    @classmethod
-    def get_all_group_names(cls, settings_list: List[Dict]):
+    @staticmethod
+    def get_all_group_names(settings_list: List[Dict]):
         """
         """
-        scraper = cls()
-        sid = cls.authenticate()
-        scraper.set_attributes(SID=sid)
+        sid = Scraper.authenticate()
+        setting = Setting(sid=sid)
 
         all_groups = {}
         for settings in settings_list:
-            print(settings)
-            scraper.set_attributes(**settings)
-            groups = scraper.get_groups()
-            all_groups[f'{settings[YEAR]}_{settings[SEMESTER]}'] = groups
+            setting.set_settings(**settings)
+            groups = Scraper.get_group_names(setting)
+            all_groups[f'{settings.year}_{settings.semester}'] = groups
 
         return all_groups
-
-
-settings_list = [
-    {YEAR: year, SEMESTER: semester}
-    for year in ['2019', '2020']
-    for semester in [WINTER, SUMMER, RETAKE]
-]
-
-if __name__ == '__main__':
-    all_group_names = Scraper.get_all_group_names(settings_list)
-    print(all_group_names)
