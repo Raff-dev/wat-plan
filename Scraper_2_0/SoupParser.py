6d21cf008a31c3d56a81d8b9fcbc98f31e191b13
Raff <56380303+Raff-dev@users.noreply.github.com>
2020-10-06 18:39:54 +0200

    Enclosed code within class | Using proper year value

--- Scraper_2_0/SoupParser.py
+++ Scraper_2_0/SoupParser.py
@@ -1,14 +1,12 @@
-import requests
-from bs4 import BeautifulSoup
 from typing import List, Dict, Iterable, TypeVar
 import types
 import inspect
 
+from bs4 import BeautifulSoup
 import pandas as pd
-import numpy as np
 import datetime
 
-from Decorators import TimeMeasure
+from Setting import Setting
 
 ROMAN_NOTATION = {
     'I': 1, 'II': 2, 'III': 3, 'IV': 4,
@@ -36,28 +34,28 @@ class SoupParser():
             self.place = None
 
     @staticmethod
-    def get_soup(url: str) -> BeautifulSoup:
-        res = requests.get(url, verify=False)
-        # --- TEST PRINT ---
-        print(res)
-        soup = BeautifulSoup(res.text, features="lxml")
-        return soup
-
-    @staticmethod
-    def get_group_schedule(url: str):
+    @Setting.requires_setting(Setting.SEMESTER, Setting.YEAR, Setting.GROUP)
+    def get_group_schedule(soup: BeautifulSoup, setting: Setting):
         """
          :param url -
          :return
         """
-        soup = SoupParser.get_soup(url)
         days_soup = SoupParser.__soup_to_sorted_days_soup(soup)
         if days_soup is None:
             return None
 
-        start_date = SoupParser.__get_start_date(soup)
+        year = int(setting.year)
+        year = year if setting.year == Setting.WINTER else year+1
+        start_date = SoupParser.__get_start_date(soup, year)
+
         group_schedule = SoupParser.__parse_semester(days_soup, start_date)
-        print(group_schedule)
-        return group_schedule.copy()
+        group_schedule_formatted = {
+            'year': setting.year,
+            'semester': setting.semester,
+            'group': setting.group,
+            'plan': group_schedule
+        }
+        return group_schedule_formatted
 
     @staticmethod
     def __soup_to_sorted_days_soup(soup: List[BeautifulSoup]) -> List[List[BeautifulSoup]]:
@@ -67,23 +65,22 @@ class SoupParser():
 
         columns_count = int(len(blocks) / (DAYS_PER_WEEK*BLOCKS_PER_DAY))
         # print(f'BLOCKS: {blocks}')
-        row_blocks = array_split(blocks, columns_count)
+        row_blocks = SoupParser.array_split(blocks, columns_count)
         # print(f'ROW BLOCKS: {row_blocks}')
         week_blocks = pd.DataFrame(row_blocks).T.values
         # print(f'WEEK BLOCKS: {week_blocks}')
         sorted_blocks = [
             block for column in week_blocks for block in column]
         # print(f'SORTED BLOCKS: {sorted_blocks}')
-        sorted_days = array_split(sorted_blocks, DAYS_PER_WEEK)
+        sorted_days = SoupParser.array_split(sorted_blocks, DAYS_PER_WEEK)
         # print(f'SORTED DAYS: {sorted_days}')
         return sorted_days
 
     @staticmethod
-    def __get_start_date(soup: BeautifulSoup) -> datetime.date:
+    def __get_start_date(soup: BeautifulSoup, year: int) -> datetime.date:
         day_month = soup.find_all(class_='thFormList1HSheTeaGrpHTM3')[0]
-        day, month = array_split(day_month.nobr.text, 2)
+        day, month = SoupParser.array_split(day_month.nobr.text, 2)
         month = ROMAN_NOTATION[month]
-        year = datetime.date.today().year
 
         return datetime.date(year, month, int(day))
 
@@ -93,7 +90,7 @@ class SoupParser():
             start_date: datetime.date):
 
         semester_schedule = {}
-        for day_data, date in enumerate_date(days_soup, start_date):
+        for day_data, date in SoupParser.enumerate_date(days_soup, start_date):
             day_schedule = SoupParser.__parse_day(day_data)
             date = str(date)
             semester_schedule[date] = day_schedule
@@ -134,33 +131,14 @@ class SoupParser():
         block.place = data[2:]
         return block.__dict__
 
+    @staticmethod
+    def array_split(iterable: Iterable[TypeVar], chunk_length: int) -> List[List[TypeVar]]:
+        if isinstance(iterable, types.GeneratorType):
+            iterable = list(iterable)
+        return [iterable[i:i+chunk_length]if len(iterable)-i != 1 else iterable[-1] for i in range(0, len(iterable), chunk_length)]
 
-def array_split(iterable: Iterable[TypeVar], chunk_length: int) -> List[List[TypeVar]]:
-    if isinstance(iterable, types.GeneratorType):
-        iterable = list(iterable)
-    return [iterable[i:i+chunk_length]if len(iterable)-i != 1 else iterable[-1] for i in range(0, len(iterable), chunk_length)]
-
-
-def enumerate_date(iterable: Iterable[TypeVar], start_date: datetime.date, days_step: int = 1):
-    for element in iterable:
-        yield element, start_date
-        start_date += datetime.timedelta(days=days_step)
-
-
-schedule = {
-    'year': '20202',
-    'semester': 'winter',
-    'group': 'WCY18IY5S1',
-    'plan': {
-        '2020.10.01': {
-            '1': {
-                'title': 'lorem ipsum',
-                'teacher': 'lorem ipsum',
-                'class_index': 'lorem ipsum',
-                'subject': 'lorem ipsum',
-                'class_type': 'lorem ipsum',
-                'place': 'lorem ipsum',
-            },
-        }
-    }
-}
+    @staticmethod
+    def enumerate_date(iterable: Iterable[TypeVar], start_date: datetime.date, days_step: int = 1):
+        for element in iterable:
+            yield element, start_date
+            start_date += datetime.timedelta(days=days_step)
