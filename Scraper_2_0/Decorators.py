ec86fe0ba9c3f45ddc1f123b8ab90faac8f42902
Raff <56380303+Raff-dev@users.noreply.github.com>
2020-10-11 12:39:17 +0200

    Code maintenance

--- Scraper_2_0/Decorators.py
+++ Scraper_2_0/Decorators.py
@@ -1,13 +1,10 @@
 import time
 
 
-def timer(repeat):
-    def wrapper(func):
-        def wrapper(*args, **kwargs):
-            start = time.time()
-            runs = [func(*args, **kwargs) for _ in range(repeat)]
-            end = time.time()
-            avg_time = (end-start)/len(runs)
-            return runs[0], avg_time
-        return wrapper
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        res = func(*args, **kwargs)
+        end = time.time()
+        return res, end-start
     return wrapper
