7c5adf5e03a34a312b89a8ad5bcd130dcbd0ceef
Raff-dev <56380303+Raff-dev@users.noreply.github.com>
2020-12-01 08:12:45 +0100

    Version Increment

--- Android/app/src/main/java/com/example/WatPlan/Handlers/UpdateHandler.java
+++ Android/app/src/main/java/com/example/WatPlan/Handlers/UpdateHandler.java
@@ -24,7 +24,7 @@ import java.util.Set;
 import static com.example.WatPlan.Models.Preferences.*;
 
 public class UpdateHandler extends Thread {
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.3";
     private DBHandler dbHandler;
     private MainActivity mainActivity;
     private ScheduleFragment scheduleFragment;
